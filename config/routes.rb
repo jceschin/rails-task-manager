efa629b94d030288827c7fa673eb2f152ce7194a
Juan Ceschin <juanceschin@gmail.com>
2020-06-13 12:30:32 -0300

    Add view for details of one task and feature to add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # List all tasks
-  get "tasks", to: "tasks#index"
+  get 'tasks', to: 'tasks#index'
+
+  # Create a task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
