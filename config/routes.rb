c98486ba5f2f33bcbb23859f8c058b4a7ce4f1ab
Juan Ceschin <juanceschin@gmail.com>
2020-06-14 14:10:41 -0300

    Add resources to create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # List all tasks
-  get 'tasks', to: 'tasks#index'
-
-  # Create a task
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-
-  # Read one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  # Update a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
-  patch 'tasks/:id', to: 'tasks#update'
-
-  # Delete a task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
